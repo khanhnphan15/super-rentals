bea18f779004c01d1fe2ac9a9c59449c5d9eed22
Khanh King <khanhnphan15@yahoo.com>
2023-09-16 09:32:27 -0400

    create ember project

---
+++ tests/integration/components/nav-bar-test.js
@@ -0,0 +1,21 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'super-rentals/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | nav-bar', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<NavBar />`);
+
+    const rootElement = document.querySelector('#ember-testing');
+
+    // Display the entire rendered DOM
+    console.log(rootElement.innerHTML);
+    assert.dom().hasText('');
+  });
+});
